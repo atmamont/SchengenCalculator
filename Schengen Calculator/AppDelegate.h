e87812349ca2d58767c5055ea150f2278c518d6c
Vit <seatechua@icloud.com>
2015-06-20 12:12:09 +0300

    Initial Commit

---
+++ Schengen Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Schengen Calculator
+//
+//  Created by Vit on 20.06.15.
+//  Copyright (c) 2015 ELTIMA LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
