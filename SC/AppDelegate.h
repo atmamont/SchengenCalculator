5d434259b712310ec22d3762c52487c49ade921d
atMamont <atMamont@Mamont-Mac.local>
2015-10-04 13:01:22 +0300

    Commit repaired project

---
+++ SC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SC
+//
+//  Created by atMamont on 17.07.15.
+//  Copyright (c) 2015 ELTIMA LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
